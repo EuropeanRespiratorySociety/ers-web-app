af886e442077f14d321f8301d978db4e73395e03
idealley <samuelpouyt@gmail.com>
2018-06-15 10:49:30 +0200

    Added feedback link in footer

--- src/components/base/SiteFooter.vue
+++ src/components/base/SiteFooter.vue
@@ -1,29 +1,9 @@
 <template>
-  <v-bottom-nav class="elevation-1" app :value="true" :active.sync="e1">
-    <!--  <v-btn flat value="recent">
-        <span>Recent</span>
-        <v-icon>history</v-icon>
+  <v-bottom-nav class="elevation-1" app :value="true">
+      <v-btn href="https://forms.ersnet.org/feedback" target="_blank" flat value="register">
+        <span>Feedback</span>
+        <v-icon>s7-chat</v-icon>
       </v-btn>
-      <v-btn flat value="favorites">
-        <span>Favorites</span>
-        <v-icon>favorite</v-icon>
-      </v-btn>
-
-      <v-bottom-sheet v-model="sheet" inset>
-      <v-btn slot="activator" flat value="register">
-        <span>Register</span>
-        <v-icon>place</v-icon>
-      </v-btn>
-      <v-container grid-list-md text-xs-center white>
-        <v-layout row  style="height:200px" justify-center>
-          <v-flex xs3 v-for="i in 3" :key="i">
-            <v-card dark color="primary">
-              <v-card-text class="px-0">4</v-card-text>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-bottom-sheet> -->
   </v-bottom-nav>
 </template>
 
@@ -31,10 +11,7 @@
 export default {
   name: 'site-footer',
   data () {
-    return {
-      e1: 'recent',
-      sheet: false
-    }
+    return {}
   }
 }
 </script>
