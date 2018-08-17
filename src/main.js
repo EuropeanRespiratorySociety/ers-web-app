5eff9c13fe1489fc98d6a88d0871c51a13976d5f
idealley <samuelpouyt@gmail.com>
2018-08-17 12:43:10 +0200

    IE 11 Fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import "babel-polyfill";
+import "@babel/polyfill";
 import "intersection-observer";
 
 import Vue from "vue";
