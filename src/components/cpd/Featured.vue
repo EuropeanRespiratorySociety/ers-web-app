b4c93e1bbd1282c335eb7e86b67333530cfd830e
idealley <samuelpouyt@gmail.com>
2018-08-22 15:12:49 +0200

    removed useless console.log

--- src/components/cpd/Featured.vue
+++ src/components/cpd/Featured.vue
@@ -3,9 +3,33 @@
     <v-content>
       <v-container fluid>
         <v-layout row>
-          <v-flex xs12 sm12 md8 lg8 offset-md2 offset-lg2>
-            <p>hello</p>
+          <v-flex xs12 sm12 md8 lg8 offset-md2 offset-lg2>  
+            <v-card>
+              <v-card-text v-html="category.body"/>
+            </v-card>
           </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <cards-swiper
+              v-if="articles.length > 0"
+              :articles="articles"
+              :parent="cpd"/>
+          </v-flex>
+        </v-layout>
+        <v-layout xs12>
+          <v-list v-for="(i, k) in interests" :key="k" subheader dense>
+            <div v-if="i.title === 'diseases'">
+              <v-list-tile v-for="(v, k) in i.values" :key="'value-' + k">
+                <v-list-tile-action>
+                  <v-checkbox v-model="prefs" :value="v"/>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>{{v}}</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </div>
+          </v-list>
         </v-layout>
       </v-container>
     </v-content>
@@ -13,16 +37,32 @@
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapState } from "vuex";
+const CardsSwiper = () => import("@/components/category/CardsSwiper.vue");
 
 export default {
+  components: {
+    CardsSwiper
+  },
+
+  data() {
+    return {
+      prefs: []
+    };
+  },
+
+  computed: {
+    ...mapState("cpd", ["category", "articles"]),
+    ...mapState("user", ["interests"])
+  },
+
   created() {
     this.fetchData();
   },
 
   metaInfo() {
     return {
-      title: `Continuing Professional development`
+      title: this.category.title
     };
   },
 
--- src/components/cpd/store/mutations.js
+++ src/components/cpd/store/mutations.js
@@ -2,11 +2,9 @@ import * as types from "./mutation-types";
 
 export default {
   [types.SET_CATEGORY](state, data) {
-    console.log("cat:", data);
-    state.category = data;
+    state.category = { ...data };
   },
   [types.SET_ARTICLES](state, data) {
-    console.log("articles", data);
     state.articles = [...data];
   }
 };
