97ee424a2f0a0ed475994244255d38d56f5135a1
idealley <samuelpouyt@gmail.com>
2018-11-29 11:10:13 +0100

    Clean up helper

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,10 +1,6 @@
 import axios from "axios";
-console.log(process.env.VUE_APP_ERS_API);
 export const HTTP = axios.create({
   baseURL: process.env.VUE_APP_ERS_API,
-  // process.env.NODE_ENV === "production"
-  //   ? "https://api.ersnet.org"
-  //   : "http://localhost:3030",
   headers: { "Content-Type": "application/json" }
 });
 
