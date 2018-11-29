89fe0ebe0ae65968023dff8d9662ae89f8de15d0
idealley <samuelpouyt@gmail.com>
2018-11-29 09:54:06 +0100

    Added env file for API source

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const HTTP = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? "https://api.ersnet.org"
-      : "http://localhost:3030",
+  baseURL: process.env.API,
+  // process.env.NODE_ENV === "production"
+  //   ? "https://api.ersnet.org"
+  //   : "http://localhost:3030",
   headers: { "Content-Type": "application/json" }
 });
 
