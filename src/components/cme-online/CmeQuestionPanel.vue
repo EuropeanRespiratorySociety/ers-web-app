6cdf8ea2ff0cc69878df19481ed3a91108e76d27
tasanoers <tamaki.asano@ersnet.org>
2019-05-06 10:46:25 +0200

    Change the text color on Answer part

--- src/components/cme-online/CmeQuestionPanel.vue
+++ src/components/cme-online/CmeQuestionPanel.vue
@@ -60,12 +60,13 @@
             </v-flex>
           </v-layout>
           <v-btn :disabled="hasAnswer" color="primary" v-on:click="showResult">Answer</v-btn>
-          <div v-if="isValidate">
-            <h3>Answer:</h3>
-            <div v-if="isCorrect">Correct !</div>
-            <div v-else>Incorrect !</div>
-
-            <p>{{solution}}</p>
+          <div v-if="isValidate" class="mt-4">
+            <div v-if="isCorrect" class="green--text text--darken-2 title">Your answer is correct!</div>
+            <div v-else class="red--text text--darken-2 title">Your answer is incorrect!</div>
+            <v-alert :value="true" color="success" icon="check_circle" outline class="mt-2 mb-4">
+              <span class="font-weight-bold">Correct answer: {{solution}}</span>
+            </v-alert>
+            <!-- <p>Correct answer: {{solution}}</p> -->
             <div v-if="hasValue(currentPanel.question.comment)">
               <h4>Comment</h4>
               <p>
