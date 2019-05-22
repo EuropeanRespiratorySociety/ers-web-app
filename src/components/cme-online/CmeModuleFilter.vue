696790d71479ccd53c493e2caf75ca23ee40914d
Florence Blocklet <florence.blocklet@ersnet.org>
2019-05-22 16:28:46 +0200

    Fix: Cme Filter color and design

--- src/components/cme-online/CmeModuleFilter.vue
+++ src/components/cme-online/CmeModuleFilter.vue
@@ -6,8 +6,20 @@
           <v-card-actions>
             <p class="subheading ml-2 mr-2 font-weight-bold">Filters</p>
             <div v-if="!showFilters" class="text-xs-left">
-              <v-chip v-for="disease in filters.diseases" :key="disease" small>{{disease}}</v-chip>
-              <v-chip v-for="method in filters.methods" :key="method" small>{{method}}</v-chip>
+              <v-chip
+                v-for="disease in filters.diseases"
+                :key="disease"
+                small
+                color="#F3E5F5"
+                text-color="#4A148C"
+              >{{disease}}</v-chip>
+              <v-chip
+                v-for="method in filters.methods"
+                :key="method"
+                small
+                color="#D0F8CE"
+                text-color="#0D5302"
+              >{{method}}</v-chip>
               <v-chip v-for="cmetype in filters.types" :key="cmetype" small>{{cmetype}}</v-chip>
               <v-chip v-for="categorie in filters.categories" :key="categorie" small>{{categorie}}</v-chip>
             </div>
@@ -23,41 +35,89 @@
                 <v-select
                   :items="filtersValues.diseases"
                   v-model="filters.diseases"
-                  chips
                   label="Diseases"
                   multiple
+                  chips
+                  return-object
                   @change="fetchCmeModulesPerFilters"
-                />
+                >
+                  <template slot="selection" slot-scope="data">
+                    <v-chip
+                      :selected="data.selected"
+                      :key="JSON.stringify(data.item)"
+                      close
+                      class="chip--select-multi"
+                      color="#F3E5F5"
+                      text-color="#4A148C"
+                      @input="data.parent.selectItem(data.item)"
+                    >{{ data.item }}</v-chip>
+                  </template>
+                </v-select>
               </v-flex>
               <v-flex xs12 sm6>
                 <v-select
                   :items="filtersValues.methods"
                   v-model="filters.methods"
-                  chips
                   label="Methods"
                   multiple
+                  chips
+                  return-object
                   @change="fetchCmeModulesPerFilters"
-                />
+                >
+                  <template slot="selection" slot-scope="data">
+                    <v-chip
+                      :selected="data.selected"
+                      :key="JSON.stringify(data.item)"
+                      close
+                      class="chip--select-multi"
+                      color="#D0F8CE"
+                      text-color="#0D5302"
+                      @input="data.parent.selectItem(data.item)"
+                    >{{ data.item }}</v-chip>
+                  </template>
+                </v-select>
               </v-flex>
               <v-flex xs12 sm6>
                 <v-select
                   :items="filtersValues.types"
                   v-model="filters.types"
-                  chips
                   label="Types"
                   multiple
+                  chips
+                  return-object
                   @change="fetchCmeModulesPerFilters"
-                />
+                >
+                  <template slot="selection" slot-scope="data">
+                    <v-chip
+                      :selected="data.selected"
+                      :key="JSON.stringify(data.item)"
+                      close
+                      class="chip--select-multi"
+                      @input="data.parent.selectItem(data.item)"
+                    >{{ data.item }}</v-chip>
+                  </template>
+                </v-select>
               </v-flex>
               <v-flex xs12 sm6>
                 <v-select
                   :items="filtersValues.categories"
                   v-model="filters.categories"
-                  chips
                   label="Categories"
                   multiple
+                  chips
+                  return-object
                   @change="fetchCmeModulesPerFilters"
-                />
+                >
+                  <template slot="selection" slot-scope="data">
+                    <v-chip
+                      :selected="data.selected"
+                      :key="JSON.stringify(data.item)"
+                      close
+                      class="chip--select-multi"
+                      @input="data.parent.selectItem(data.item)"
+                    >{{ data.item }}</v-chip>
+                  </template>
+                </v-select>
               </v-flex>
             </v-layout>
           </v-container>
