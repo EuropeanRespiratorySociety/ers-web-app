acca98bf4428b1478e4b55670118eddfd59487f1
fblocklet <43543975+fblocklet@users.noreply.github.com>
2019-08-09 11:18:12 +0200

    Fix: Delete header and footer for cme online

--- src/components/cme-online/CmeOnlineApp.vue
+++ src/components/cme-online/CmeOnlineApp.vue
@@ -1,14 +1,13 @@
 <script>
-const Header = () => import("./CmeHeader.vue");
+//const Header = () => import("./CmeHeader.vue");
 const CmeLandingCarousel = () => import("./CmeLandingCarousel.vue");
 const CmeLandingPage = () => import("./CmeLandingPage.vue");
-const SiteFooter = () => import("@/components/base/SiteFooter.vue");
 
 export default {
   name: "cme-app",
   functional: true,
   render(h) {
-    return [h(Header), h(CmeLandingCarousel), h(CmeLandingPage), h(SiteFooter)];
+    return [h(CmeLandingCarousel), h(CmeLandingPage)];
   }
 };
 </script>
